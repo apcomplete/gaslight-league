d49a62209770472d74eced73efabee99a00584a4
Alex Padgett <padgett.alex@gmail.com>
2016-07-26 15:11:34 -0400

    Install simple_form

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'bootstrap-sass'
 gem 'devise'
 gem 'jquery-rails'
 gem 'sass-rails', '~> 5.0'
+gem 'simple_form'
 gem 'uglifier', '>= 1.3.0'
 gem 'unicorn-rails'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, skip: [:registrations, :password]
   root to: 'application#index'
+  resources :admin, only: [:index] do
+    collection do
+      resources :trainers, only: [:edit, :update]
+    end
+  end
 end
